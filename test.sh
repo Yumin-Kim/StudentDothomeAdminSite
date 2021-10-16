dcedef3a665caf0856c8275657a6290359607b49
Yumin-Kim <dbals0@naver.com>
2021-10-16 22:13:14 +0900

    리포지토리 간략하게 정리

--- test.sh
+++
@@ -1,4 +0,0 @@
-From https://github.com/Yumin-Kim/StudentDothomeAdminSite
- * branch            master     -> FETCH_HEAD
-./shellscript/Git.sh: line 26: syntax error near unexpected token `fi'
-./shellscript/Git.sh: line 26: `fi'
--- util.sh
+++
@@ -1,8 +0,0 @@
-
-echo Hello >> /home/oem/WebDir/StudentDothomeAdminSite/hello.txt
-
-#현재시각을 담아둘 변수
-nowdate=$(date +"%Y-%m-%d %H:%M:%S")
-
-# 텍스트와 crontab이 수행될 시각을 같이 표현해서 check_test라는 파일에 작성한다.
-echo "It is ${nowdate}, crontab test success." >> /home/oem/WebDir/StudentDothomeAdminSite/hello.txt
